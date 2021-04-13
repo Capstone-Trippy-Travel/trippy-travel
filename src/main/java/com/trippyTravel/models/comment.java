f0067cb82a1797921269d9e84f0f0129a1ed60a3
kuwan barnes <Kuwan.barne@gmail.com>
2021-04-13 09:42:21 -0500

    first merge

--- src/main/java/com/trippyTravel/models/comment.java
+++ src/main/java/com/trippyTravel/models/comment.java
@@ -1,8 +1,12 @@
 package com.trippyTravel.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "comment")
 public class comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
 }
