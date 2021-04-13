6d41049eecba792cd4c384c257673d38590a2ab9
kuwan barnes <Kuwan.barne@gmail.com>
2021-04-13 09:57:53 -0500

    comment finish

--- src/main/java/com/trippyTravel/models/comment.java
+++ src/main/java/com/trippyTravel/models/comment.java
@@ -1,5 +1,7 @@
 package com.trippyTravel.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,4 +11,58 @@ public class comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column( name= "comment_text", length = 100)
+    private String comment_text;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonManagedReference
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id")
+    @JsonManagedReference
+    private Trip trip;
+
+    public comment(){}
+
+    public comment(long id, String comment_text, User user, Trip trip) {
+        this.id = id;
+        this.comment_text = comment_text;
+        this.user = user;
+        this.trip = trip;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment_text() {
+        return comment_text;
+    }
+
+    public void setComment_text(String comment_text) {
+        this.comment_text = comment_text;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+    }
 }
