c7409c6cbe10582672ec718c4b491de333e9813b
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-04-13 13:52:05 -0500

    adding group controller

--- src/main/java/com/trippyTravel/controllers/GroupController.java
+++ src/main/java/com/trippyTravel/controllers/GroupController.java
@@ -1,4 +1,11 @@
 package com.trippyTravel.controllers;
 
+import com.trippyTravel.repositories.GroupMembersRepository;
+import com.trippyTravel.repositories.GroupsRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class GroupController {
+    private GroupsRepository groupDao;
+    private GroupMembersRepository groupMemberDao;
 }
