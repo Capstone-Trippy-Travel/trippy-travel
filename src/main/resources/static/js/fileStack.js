672996bf50d442d8d40017c7160e16aae1b21f69
kuwan barnes <Kuwan.barne@gmail.com>
2021-04-14 19:37:18 -0500

    fixing filestack

--- src/main/resources/static/js/fileStack.js
+++ src/main/resources/static/js/fileStack.js
@@ -1,4 +1,15 @@
-"use strict";
-
-const client = filestack.init(api);
-client.picker().open();
+// "use strict";
+//
+// const client = filestack.init(api);
+// client.picker().open();
+//
+// window.addEventListener('DOMContentLoaded', function () {
+//     const client = filestack.init(api);
+//     const options = {
+//         maxFiles: 20,
+//         uploadInBackground: false,
+//         onOpen: () => console.log('uploading pictures!'),
+//         onUploadDone: (res) => console.log(res),
+//     };
+//     client.picker(options).open();
+// });
