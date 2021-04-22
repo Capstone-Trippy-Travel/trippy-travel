65ad2ef3ce2c7b517d85dd64411e1909b4cb048e
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-04-22 08:46:15 -0500

    updating modal picture display style

--- src/main/resources/static/js/alternateModal.js
+++ src/main/resources/static/js/alternateModal.js
@@ -4,8 +4,8 @@ var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 // var img = $(".myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var modalImg = document.getElementsByTagName("img");
+var captionText = document.getElementsByTagName("p");
 function image(event)  {
     modal.style.display = "block";
     modalImg.src = event.target.src;
