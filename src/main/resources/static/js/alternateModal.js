e39a8b1e99bf0d45c786f5469059c85c0d1ef191
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-04-21 20:27:16 -0500

    updating user profile and group profile

---
+++ src/main/resources/static/js/alternateModal.js
@@ -0,0 +1,21 @@
+"use strict";
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = $(".myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+function image(event)  {
+    modal.style.display = "block";
+    modalImg.src = event.target.src;
+    captionText.innerHTML = event.target.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
