0235d242bd293ad4645d2006a210a7545599bbe5
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-04-28 08:55:42 -0500

    closing div on trip page view

--- src/main/java/com/trippyTravel/repositories/ImageRepository.java
+++ src/main/java/com/trippyTravel/repositories/ImageRepository.java
@@ -4,9 +4,14 @@ import com.trippyTravel.models.Image;
 import com.trippyTravel.models.Trip;
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
 
+//    @Query("select i from images i, Trip p, Group g where  ")
+//    List<Image> getTripImages();
 }
