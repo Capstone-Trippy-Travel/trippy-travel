9d9838b9a0643f3de915fd6d0cca62d7285ea3d0
kuwan barnes <Kuwan.barne@gmail.com>
2021-04-28 10:34:41 -0500

    better plane

--- src/main/resources/static/js/tripImage.js
+++ src/main/resources/static/js/tripImage.js
@@ -8,7 +8,7 @@ console.log(brokeTrip[0].src);
 brokeTrip.forEach(replaceEmptySrc2);
 function replaceEmptySrc2(brokeTrip) {
     if (brokeTrip.getAttribute('src') === '') {
-        brokeTrip.src = '/imgs/trip_airplane.jpg';
+        brokeTrip.src = '/imgs/gray_plane.png';
     }
 }
 const brokeimg = document.querySelectorAll('.profile-icon-small');
