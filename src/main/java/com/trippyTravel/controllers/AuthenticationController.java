6e581a017c246e2b4dd2992552c6782e9be8af93
kuwan barnes <Kuwan.barne@gmail.com>
2021-05-03 14:27:10 -0500

    nav on forgot usernamae

--- src/main/java/com/trippyTravel/controllers/AuthenticationController.java
+++ src/main/java/com/trippyTravel/controllers/AuthenticationController.java
@@ -47,7 +47,11 @@ public class AuthenticationController {
         return "login";
     }
     @GetMapping("/login/forgot_password")
-    public String forgotPassword(){
+    public String forgotPassword(Model m){
+        List<FriendList> friendRequests= new ArrayList<>();
+        m.addAttribute("friendRequests", friendRequests);
+        List<Trip> unreadCommentTrips = new ArrayList<>();
+        m.addAttribute("unreadCommentTrips", unreadCommentTrips);
 
 
         return "forgot_password";
